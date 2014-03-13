6826ed89353bb54ecb5e81d0cb85a0438b503864
Sean Roberts <roberts.sean@gmail.com>
2014-03-13 12:47:12 -0400

    Add an install generator

---
+++ lib/generators/tenon/install/install_generator.rb
@@ -0,0 +1,18 @@
+module Tenon
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../../', __FILE__)
+
+      def copy_files
+        copy_file('config/initializers/tenon.rb', File.join(Rails.root, 'config', 'initializers', 'tenon.rb'))
+        copy_file('app/views/tenon/shared/_main_nav.html.haml', File.join(Rails.root, 'app', 'views', 'tenon', 'shared', '_main_nav.html.haml'))
+        copy_file('app/assets/stylesheets/tenon/colors-custom.css.scss', File.join(Rails.root, 'app', 'assets', 'stylesheets', 'tenon', 'colors-custom.css.scss'))
+
+        puts ''
+        puts 'Make sure you add the following lines of code: '
+        puts '  config/application.rb: require "active_record/railtie"'
+        puts '  app/controllers/application_controller.rb: helper Tenon::Engine.helpers'
+      end
+    end
+  end
+end
