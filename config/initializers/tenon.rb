591c075789068dcf67a6b6913ceade6d81b80fbd
Sean Roberts <roberts.sean@gmail.com>
2014-03-13 12:47:01 -0400

    Move requires to the gem itself

--- config/initializers/tenon.rb
+++ config/initializers/tenon.rb
@@ -1,15 +1,3 @@
-require 'tenon.rb'
-require 'tenon/config/events.rb'
-require 'tenon/can_be_foreign.rb'
-require 'tenon/can_have_comments.rb'
-require 'tenon/has_asset.rb'
-require 'tenon/i18n_lookup.rb'
-require 'tenon/proxy_attachment.rb'
-require 'tenon/asset_style_generator.rb'
-require 'tenon/reorderable.rb'
-require 'tenon/tenon_content.rb'
-require 'tenon/warning_generator.rb'
-
 # This configuration block is for config that should be specified
 # by the site developer.  There is a Settings module built into Tenon
 # where you can expose other settings to your client.
--- lib/tenon.rb
+++ lib/tenon.rb
@@ -1,4 +1,14 @@
 require 'tenon/engine'
+require 'tenon/config/events.rb'
+require 'tenon/can_be_foreign.rb'
+require 'tenon/can_have_comments.rb'
+require 'tenon/has_asset.rb'
+require 'tenon/i18n_lookup.rb'
+require 'tenon/proxy_attachment.rb'
+require 'tenon/asset_style_generator.rb'
+require 'tenon/reorderable.rb'
+require 'tenon/tenon_content.rb'
+require 'tenon/warning_generator.rb'
 
 module Tenon
   class << self
