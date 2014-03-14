9eafcb9bc3c5675218d4688450a20ea42589e549
Sean Roberts <roberts.sean@gmail.com>
2014-03-14 12:51:11 -0400

    Move the host check for comment mailer into the view

--- app/mailers/tenon/comment_mailer.rb
+++ app/mailers/tenon/comment_mailer.rb
@@ -1,12 +1,16 @@
 module Tenon
   class CommentMailer < ActionMailer::Base
-    default :from => MySettings.from_email
-    default_url_options[:host] = MySettings.site_url ? MySettings.site_url.gsub('http://', '') : 'localhost:3000'
+    # TODO: Find a better way to set the host
+    # default_url_options[:host] = MySettings.site_url ? MySettings.site_url.gsub('http://', '') : 'localhost:3000'
 
     def comment_notification(email, comment)
       @post = comment.commentable
       @comment = comment
-      mail(:to => email, :subject => "#{MySettings.site_name} - New Comment: #{@post.title}")
+      mail(
+        to: email,
+        subject: "#{Tenon::MySettings.site_name} - New Comment: #{@post.title}",
+        from: Tenon::MySettings.from_email
+      )
     end
   end
 end
