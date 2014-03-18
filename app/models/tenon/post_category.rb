6d6a9f43881295dc81a65b9ae4c5d87ad3a4df8d
Sean Roberts <roberts.sean@gmail.com>
2014-03-18 12:15:45 -0400

    Add feature specs for Post Categories

--- app/models/tenon/post_category.rb
+++ app/models/tenon/post_category.rb
@@ -2,5 +2,6 @@ module Tenon
   class PostCategory < ActiveRecord::Base
     has_and_belongs_to_many :posts
     validates_uniqueness_of :title
+    validates_presence_of :title
   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
     title 'Test Post'
     excerpt 'Lorem ipsum test post'
   end
+
+  factory :post_category, class: Tenon::PostCategory do
+    sequence(:title) { |n| "Test Post Category - #{n}" }
+  end
 end
