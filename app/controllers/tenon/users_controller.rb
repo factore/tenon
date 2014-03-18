d210522adb17d9f2a0ba11451e86db076915974c
Sean Roberts <roberts.sean@gmail.com>
2014-03-17 20:46:00 -0400

    Don't allow admins to turn themselves or other users into super_admins

--- app/controllers/tenon/users_controller.rb
+++ app/controllers/tenon/users_controller.rb
@@ -9,7 +9,7 @@ module Tenon
           if current_user.is_super_admin?
             @users = Tenon::User.all
           elsif current_user.is_admin?
-            @users = Tenon::User.includes(:roles).where.not(tenon_roles: { title: 'Super Admin' })
+            @users = Tenon::User.exclude_super_admins
           end
 
           @users = @users.where(search_args) unless params[:q].blank?
@@ -30,7 +30,27 @@ module Tenon
     end
 
     def resource_params
-      params.require(:user).permit!
+      if current_user.is_admin? || current_user.is_super_admin?
+        cleaned_role_ids(params.require(:user).permit!)
+      else
+        untrusted_params
+      end
+    end
+
+    def cleaned_role_ids(rp)
+      sa_role = Tenon::Role.find_by_title('Super Admin')
+      if sa_role && rp[:role_ids] && !current_user.is_super_admin?
+        rp[:role_ids].delete(sa_role.id.to_s)
+      end
+      rp
+    end
+
+    def untrusted_params
+      params.require(:user).permit(
+        :email,
+        :password,
+        :password_confirmation
+      )
     end
   end
 end
--- spec/controllers/tenon/users_controller_spec.rb
+++ spec/controllers/tenon/users_controller_spec.rb
@@ -30,7 +30,7 @@ describe Tenon::UsersController do
 
   describe 'GET index.json' do
     before do
-      [:all, :includes, :where, :not, :paginate].each do |query|
+      [:all, :exclude_super_admins, :where, :paginate].each do |query|
         Tenon::User.stub(query) { Tenon::User }
       end
     end
@@ -50,9 +50,7 @@ describe Tenon::UsersController do
       # Proper settings from let statement at top
 
       it 'should get everyone but the super admins' do
-        expect(Tenon::User).to receive(:includes).with(:roles)
-        expect(Tenon::User).to receive(:where)
-        expect(Tenon::User).to receive(:not).with(tenon_roles: { title: 'Super Admin' })
+        expect(Tenon::User).to receive(:exclude_super_admins)
         get :index, format: 'json'
       end
     end
@@ -68,6 +66,47 @@ describe Tenon::UsersController do
     it 'should paginate the users' do
       expect(Tenon::User).to receive(:paginate)
       get :index, format: 'json'
+    end
+  end
+
+  describe 'POST create' do
+    context 'when the user is an admin' do
+      it 'should not allow the user to assign a super_admin role' do
+        super_admin_role = Tenon::Role.create(title: 'Super Admin')
+        post :create, user: { role_ids: [super_admin_role.id] }
+        expect(assigns[:user].role_ids).not_to include(super_admin_role.id)
+      end
+    end
+
+    context 'when the user is a super_admin' do
+      before { user.stub(:is_super_admin?) { true } }
+      it 'should allow the user to assign a super_admin role' do
+        super_admin_role = Tenon::Role.create(title: 'Super Admin')
+        post :create, user: { role_ids: [super_admin_role.id] }
+        expect(assigns[:user].role_ids).to include(super_admin_role.id)
+      end
+    end
+  end
+
+  describe 'PUT update' do
+    let(:edited_user) { Tenon::User.new }
+    before { Tenon::User.stub(:find) { edited_user } }
+
+    context 'when the user is an admin' do
+      it 'should not allow the user to assign a super_admin role' do
+        super_admin_role = Tenon::Role.create(title: 'Super Admin')
+        put :update, id: 1, user: { role_ids: [super_admin_role.id] }
+        expect(assigns[:user].role_ids).not_to include(super_admin_role.id)
+      end
+    end
+
+    context 'when the user is a super_admin' do
+      before { user.stub(:is_super_admin?) { true } }
+      it 'should allow the user to assign a super_admin role' do
+        super_admin_role = Tenon::Role.create(title: 'Super Admin')
+        put :update, id: 1, user: { role_ids: [super_admin_role.id] }
+        expect(assigns[:user].role_ids).to include(super_admin_role.id)
+      end
     end
   end
 end
