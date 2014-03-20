d68197a254471460880f9bdbeb753f927b8ee40d
Sean Roberts <roberts.sean@gmail.com>
2014-03-20 12:08:43 -0400

    Fix gif cropping

--- lib/paperclip_processors/cropper.rb
+++ lib/paperclip_processors/cropper.rb
@@ -2,7 +2,7 @@ module Paperclip
   class Cropper < Thumbnail
     def transformation_command
       if @attachment.instance.cropping?
-        crop_command + super.join(' ').sub(/ -crop \S+/, '').split(' ') # super returns an array like this: ["-resize", "100x", "-crop", "100x100+0+0", "+repage"]
+        crop_command + super.join(' ').sub(/ -crop \S+/, '').split(' ')
       else
         super
       end
@@ -11,7 +11,11 @@ module Paperclip
     def crop_command
       target = @attachment.instance
       if target.cropping?
-        ['-crop', "#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}"]
+        [
+          '-crop',
+          "#{target.crop_w.to_i}x#{target.crop_h.to_i}+#{target.crop_x.to_i}+#{target.crop_y.to_i}",
+          '+repage'
+        ]
       end
     end
   end
