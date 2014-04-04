e311fa7c6fadddeac178fa7a8151e62c145d1646
Chad Ingles <chad@factore.ca>
2014-04-04 09:32:42 -0400

    added two and three column image row types

--- app/models/tenon/tenon_content/row_types/two_column_image.rb
+++ app/models/tenon/tenon_content/row_types/two_column_image.rb
@@ -5,8 +5,8 @@ module Tenon
         class << self
           def add_pieces_to(row)
             if row.pieces.empty?
-              row.pieces.build(piece_type: 'Image', position: 0)
-              row.pieces.build(piece_type: 'Image', position: 1)
+              row.pieces.build(piece_type: 'Image', position: 0, size: 'six')
+              row.pieces.build(piece_type: 'Image', position: 1, size: 'six')
             end
           end
         end
