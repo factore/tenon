65453dea9466f18ee47953f9ed09e7c73d4841f5
Jamie Allen <jamie@factore.ca>
2014-04-15 10:59:13 -0400

    working contact decorator specs

--- spec/decorators/tenon/contact_decorator_spec.rb
+++ spec/decorators/tenon/contact_decorator_spec.rb
@@ -3,31 +3,12 @@ require 'spec_helper'
 describe Tenon::ContactDecorator do
   let (:cd) { Tenon::ContactDecorator.new(contact) }
 
-  describe '#email' do
-    let(:contact) { double(author_email: 'test@test.com') }
-    it 'should get the author email from the object' do
-      expect(cd.email).to eq('test@test.com')
-    end
-  end
-
-  describe '#author_url' do
-    ['http://', 'https://'].each do |protocol|
-      context "when the author has started their URL with #{protocol}" do
-        let(:contact) { double(author_url: "#{protocol}test.com") }
-
-        it 'should use the author_url from the object' do
-          expect(cd.author_url).to eq(contact.author_url)
-        end
-      end
+  describe '#sent_date' do
+    let(:contact) { double(created_at: Time.new(2014, 03, 26)) }
+    it 'should return created_at as %B %e, %Y at %l:%M %p' do
+      expect(cd.sent_date).to eq( Time.new(2014, 03, 26).strftime('%B %e, %Y at %l:%M %p') )
     end
 
-    context 'when the author has not included a protocol' do
-      let(:contact) { double(author_url: 'test.com') }
-
-      it "should prepend 'http://' to the url" do
-        expect(cd.author_url).to eq("http://#{contact.author_url}")
-      end
-    end
   end
 
   describe '#error_message' do
