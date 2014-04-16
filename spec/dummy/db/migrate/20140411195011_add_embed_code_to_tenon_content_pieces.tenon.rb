9103854420dcb7f89a42d27fbb0234ff19fbbec9
Sean Roberts <roberts.sean@gmail.com>
2014-04-15 22:08:18 -0400

    Keep dummy migrations out of the repo

--- spec/dummy/db/migrate/20140411195011_add_embed_code_to_tenon_content_pieces.tenon.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from tenon (originally 20140401191354)
-class AddEmbedCodeToTenonContentPieces < ActiveRecord::Migration
-  def change
-    add_column :tenon_tenon_content_pieces, :embed_code, :text
-  end
-end
--- spec/dummy/db/migrate/20140415172352_add_contact_form_flag_to_pages.rb
+++
@@ -1,5 +0,0 @@
-class AddContactFormFlagToPages < ActiveRecord::Migration
-  def change
-    add_column :tenon_pages, :show_contact_form, :boolean, default: false
-  end
-end
