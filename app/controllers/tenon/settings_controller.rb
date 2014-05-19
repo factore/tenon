9592031ce0b00a344efff5cc37829c4d47cfc390
Chad Ingles <chad@factore.ca>
2014-05-19 11:30:18 -0400

    remove some unused code from settings_controller.rb

--- app/controllers/tenon/settings_controller.rb
+++ app/controllers/tenon/settings_controller.rb
@@ -2,7 +2,6 @@ module Tenon
   class SettingsController < Tenon::BaseController
     def update
       authorize! :update, Tenon::MySettings
-      # force_boolean(params[:settings])
       params[:settings].each { |key, value| Tenon::MySettings.send((key + '=').to_sym, value) }
       flash[:notice] = 'Your settings have been saved.'
       redirect_to settings_path
@@ -12,18 +11,5 @@ module Tenon
       authorize! :update, Tenon::MySettings
       @settings = Tenon::MySettings
     end
-
-    # private
-
-    # we want some settings to be typecast into boolean values
-    # def force_boolean(settings)
-    #   [:ecommerce_live, :use_coupons, :flat_rate_shipping, :integrated_payments].each do |key|
-    #     if settings[key] == 'true'
-    #       settings[key] = true
-    #     else
-    #       settings[key] = false
-    #     end
-    #   end
-    # end
   end
 end
