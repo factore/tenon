384e39c294b63345212ed92221afed90101508d2
Chad Ingles <chad@factore.ca>
2015-01-02 16:12:47 -0500

    next attempt at responsive images for regular assets (not tenon content)

--- app/helpers/tenon/asset_helper.rb
+++ app/helpers/tenon/asset_helper.rb
@@ -42,7 +42,23 @@ module Tenon
       end
     end
 
+    def responsive_image_tag(asset, size, options = {})
+      srcset = generate_srcset(asset)
+      sizes = generate_sizes(size)
+      image_tag(asset.attachment.url(:x2000), options.merge(srcset: srcset, sizes: sizes))
+    end
+
     private
+      def generate_sizes(size)
+        "(min-width: 768px) #{size}vw, 100vw"
+      end
+
+      def generate_srcset(asset)
+        (1..10).map do |n|
+          "#{asset.attachment.url("x#{n*200}".to_sym)} #{(n*200).to_s}w"
+        end.join(', ')
+      end
+
       def crop_options(asset)
         {
           class: 'asset-crop',
--- app/helpers/tenon/piece_helper.rb
+++ app/helpers/tenon/piece_helper.rb
@@ -8,31 +8,30 @@ module Tenon
       piece_image_tag(piece, options, Tenon.config.front_end[:breakpoints])
     end
 
-    def piece_image_tag(piece, options = {}, breakpoints)
-      srcset = generate_srcset(piece)
-      sizes = generate_sizes(piece, breakpoints)
-      image_tag(piece.image.url(:original), options.merge(srcset: srcset, sizes: sizes))
-    end
-
     private
+      def piece_image_tag(piece, options = {}, breakpoints)
+        srcset = generate_srcset(piece)
+        sizes = generate_sizes(piece, breakpoints)
+        image_tag(piece.image.url(:x2000), options.merge(srcset: srcset, sizes: sizes))
+      end
 
-    def generate_sizes(piece, breakpoints)
-      piece_size = piece.size == '' ? 'twelve' : piece.size
-      breakpoints.map do |name, sizes|
-        "(min-width: #{sizes[:browser]}px) #{(piece.sizes[piece_size.to_sym]/12*100*sizes[:content]/sizes[:browser]).to_i}vw"
-      end.join(', ')
-    end
+      def generate_sizes(piece, breakpoints)
+        piece_size = piece.size == '' ? 'twelve' : piece.size
+        breakpoints.map do |name, sizes|
+          "(min-width: #{sizes[:browser]}px) #{(piece.sizes[piece_size.to_sym]/12*100*sizes[:content]/sizes[:browser]).to_i}vw"
+        end.join(', ')
+      end
 
-    def generate_srcset(piece)
-      piece.styles.map do |name, width|
-        if piece.image.attachment.exists?(computed_style(piece, name).to_sym)
-          "#{piece.image.attachment.url(computed_style(piece, name).to_sym)} #{width}w"
-        end
-      end.join(', ')
-    end
+      def generate_srcset(piece)
+        piece.styles.map do |name, width|
+          if piece.image.attachment.exists?(computed_style(piece, name).to_sym)
+            "#{piece.image.attachment.url(computed_style(piece, name).to_sym)} #{width}w"
+          end
+        end.join(', ')
+      end
 
-    def computed_style(piece, style)
-      computed_style = "#{piece.image.style_prefix}_#{style}"
-    end
+      def computed_style(piece, style)
+        computed_style = "#{piece.image.style_prefix}_#{style}"
+      end
   end
 end
