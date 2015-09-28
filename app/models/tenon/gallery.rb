21706d2247ebdeac9a471246431943195092d4bc
Chad Ingles <chad@factore.ca>
2015-09-28 19:08:53 -0400

    add dependent_destroy to galler has_many photos

--- app/models/tenon/gallery.rb
+++ app/models/tenon/gallery.rb
@@ -1,7 +1,7 @@
 module Tenon
   class Gallery < ActiveRecord::Base
     include Tenon::Reorderable
-    has_many :photos, inverse_of: :gallery
+    has_many :photos, inverse_of: :gallery, dependent: :destroy
     accepts_nested_attributes_for :photos, allow_destroy: true, reject_if: ->(p) { p[:file_id].blank? }
     validates_presence_of :title
     validates_uniqueness_of :title
