4280cf3e765ec4c9fb9c2ba75048cc6f51ef3f95
Sean Roberts <roberts.sean@gmail.com>
2015-11-19 09:46:56 -0500

    Some set up for deploying to a server

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authenticate
+
+  def authenticate
+    if request.host =~ /\A[A-Za-z0-9-].+\.ocean\.factore\.ca\Z/
+      authenticate_or_request_with_http_basic('Tenon') do |username, password|
+        username == 'tenon' && password == 'mightyoak'
+      end
+    end
+  end
 end
