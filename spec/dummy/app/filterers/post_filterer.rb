979c687c553a518f6525eadb5ad6718472cb8561
Sean Roberts <roberts.sean@gmail.com>
2016-02-19 15:40:37 -0500

    Convenience methods in the filterer

--- spec/dummy/app/filterers/post_filterer.rb
+++ spec/dummy/app/filterers/post_filterer.rb
@@ -1,19 +1,7 @@
 class PostFilterer < Tenon::GenericFilterer #:nodoc:
   def filter
-    @scope = filter_start_date
-    @scope = filter_end_date
+    @scope = gte('posts.publish_at', params[:start_date])
+    @scope = lte('posts.publish_at', params[:end_date])
     super
-  end
-
-  private
-
-  def filter_start_date
-    return scope unless params[:start_date].present?
-    scope.where('publish_at >= ?', params[:start_date])
-  end
-
-  def filter_end_date
-    return scope unless params[:end_date].present?
-    scope.where('publish_at <= ?', params[:end_date])
   end
 end
