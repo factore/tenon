f91de9cce7f7340696a23b31fb39eb967bb77be5
Sean Roberts <roberts.sean@gmail.com>
2016-02-23 13:47:31 -0500

    Add sort functionality to the filterer

--- spec/dummy/app/filterers/post_filterer.rb
+++ spec/dummy/app/filterers/post_filterer.rb
@@ -2,6 +2,13 @@ class PostFilterer < Tenon::GenericFilterer #:nodoc:
   def filter
     @scope = gte('posts.publish_at', params[:start_date])
     @scope = lte('posts.publish_at', params[:end_date])
+    @scope = sort(params[:sort_field], params[:sort_direction])
     super
+  end
+
+  private
+
+  def allowed_sort_fields
+    ['publish_at']
   end
 end
