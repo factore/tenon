49c90eb43967da057c62feae98807198fdefbd76
Jamie Allen <jamie@factore.ca>
2016-02-23 15:02:50 -0500

    main navigation BEM'd

--- app/assets/javascripts/tenon/features/nav-item-toggle.js.coffee
+++
@@ -1,7 +0,0 @@
-class Tenon.features.NavItemToggle
-  constructor: ->
-    $('.nav-item-toggle').on('click', @_toggle)
-
-  _toggle: (e) ->
-    e.preventDefault()
-    $(e.currentTarget).closest('li').toggleClass('open')
--- app/helpers/tenon/nav_helper.rb
+++ app/helpers/tenon/nav_helper.rb
@@ -3,23 +3,23 @@ module Tenon
     def nav_item(text, path, icon, link_opts = {}, &blk)
       content = link(text, path, icon, link_opts)
       content += subnav(&blk) if block_given?
-      content_tag(:li, content, class: active_class(path, link_opts[:active]))
+      content_tag(:li, content, class: "main-nav__item #{active_class(path, link_opts[:active])}")
     end
 
     private
 
     def subnav(&blk)
-      icons = content_tag(:i, 'add', class: 'material-icon nav-open')
-      icons += content_tag(:i, 'remove', class: 'material-icon nav-close')
-      content = link_to(icons, '#', class: 'nav-item-toggle')
-      content += content_tag(:ul, capture(&blk))
+      icons = content_tag(:i, 'add', class: 'main-nav__open-icon material-icon')
+      icons += content_tag(:i, 'remove', class: 'main-nav__close-icon material-icon')
+      content = link_to(icons, '#', class: 'main-nav__link-toggle action-icon')
+      content += content_tag(:ul, capture(&blk), class: 'main-nav__sub-nav')
       content
     end
 
     def link(text, path, icon, opts = {})
-      content = content_tag(:i, icon, class: "material-icon")
-      content += text
-      opts[:class] ||= ''
+      content = content_tag(:i, icon, class: 'material-icon')
+      content += content_tag(:span, text)
+      opts[:class] ||= 'main-nav__link action-icon'
       opts[:title] ||= text
       link_to(content, path, opts)
     end
