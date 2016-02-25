4902f6be81f8b79c6ab37c36e9476ef5377f749d
Jamie Allen <jamie@factore.ca>
2016-02-25 15:11:03 -0500

    form builder tweaks, helpers and styles

---
+++ app/helpers/tenon/input_block_helper.rb
@@ -0,0 +1,25 @@
+module Tenon
+  module InputBlockHelper
+    def input_block(label, opts = {}, &blk)
+      content_tag :div, class: 'input-block' do
+        block_label_tag(label) +
+        capture(&blk) +
+        block_explanation(opts[:explanation])
+      end
+    end
+
+    private
+
+    def block_label_tag(text)
+      content_tag(:label, text, class: 'input-block__label')
+    end
+
+    def block_explanation(text)
+      if text.present?
+        return content_tag(:div, text, class: 'input-block__explanation')
+      else
+        return ''
+      end
+    end
+  end
+end
