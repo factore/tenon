3af77e51dc1799e29f4e6067c968db263065f0b1
Sean Roberts <roberts.sean@gmail.com>
2016-03-09 17:21:58 -0500

    Change flashes generated by flash_helper

--- app/helpers/tenon/flash_helper.rb
+++ app/helpers/tenon/flash_helper.rb
@@ -1,12 +1,10 @@
 module Tenon
   module FlashHelper
     def flash_messages
-      close_icon = content_tag(:i, 'close', class: 'material-icon')
       messages = []
       %w(notice alert warning error).each do |msg|
         unless flash[msg.to_sym].blank?
-          content = content_tag(:p, flash[msg.to_sym])
-          content += link_to close_icon, '#', class: 'flash__dismiss action-icon'
+          content = content_tag(:div, flash[msg.to_sym], class: 'flash__message')
           messages << content_tag(:div, content, class: "flash flash--#{msg}")
         end
       end
