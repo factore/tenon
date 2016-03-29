22c67c96664a262340110264a3c9346605092f3e
Sean Roberts <roberts.sean@gmail.com>
2016-03-29 12:00:13 -0400

    Redirect to tenon when logging into tenon

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate
+  before_filter :store_location
 
   layout :layout_for_resource
 
@@ -16,6 +17,22 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def store_location
+    # store last url - this is needed for post-login redirect to
+    # whatever the user last visited.
+    return unless request.get?
+    if  !request.path.match('/users/sign_in') &&
+        !request.path.match('/users/sign_up') &&
+        !request.path.match('/users/password/new') &&
+        !request.path.match('/users/password/edit') &&
+        !request.path.match('/users/confirmation') &&
+        !request.path.match('/users/sign_out') &&
+        !request.path.match(%r{^api\/v1\/.*}) &&
+        !xhr_or_js? # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
+
   protected
 
   def layout_for_resource
@@ -24,5 +41,9 @@ class ApplicationController < ActionController::Base
     else
       'tenon/application'
     end
+  end
+
+  def after_sign_in_path_for(_)
+    session[:previous_url] || edit_profile_path
   end
 end
